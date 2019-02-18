9e32bd149fcec2643277f13dc60b34162f08dd9b
pheno03 <46960390+pheno03@users.noreply.github.com>
2019-02-18 11:14:48 +0100

    premier list[18/02/2019]

--- CollectionsJava/src/com/exemple/listExo/TestList.java
+++ CollectionsJava/src/com/exemple/listExo/TestList.java
@@ -1,21 +1,21 @@
 package com.exemple.listExo;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class TestList {
 
 	public static void main(String[] args) {
-	
+
 		ArrayList list = new ArrayList();
-		 
-		 list.add(12);
-		 list.add("Une chaine de caractere !");
-		 list.add(12.20f);
-		 list.add('d');
-		 		 
-		 for (int i = 0; i < list.size(); i++) {
-			 System.out.println("une donnée à l'indice  "+ i + " = " +list.get(i));
-			
+		list.add(12);
+		list.add("Une chaine de caractere !");
+		list.add(12.20f);
+		list.add('d');
+
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println("une donnée à l'indice  " + i + " = " + list.get(i));
+
 		}
 
 	}
